cd1b29eda1f38dfbb4c1ee6b682c429d1c095485
Phone Myat Kyaw <phonemyatt31@Phones-MacBook-Air.local>
2025-05-30 12:15:47 -0700

    changes in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,32 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import blowfishIcon from '../assets/blowfish_icon.png';
+import { Button } from '@/components/ui/button';
+import './Dashboard.css';
+
 const Dashboard = () =>  {
+
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className='min-h-screen'>
+            {/* Navigation */}
+            <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
+                <div className="flex items-center space-x-2">
+                <img src={blowfishIcon} alt="Blowfish icon" className="h-12 w-12"/>
+                <span className="text-2xl font-bold text-gray-900">Blowfish Budget</span>
+                </div>
+                
+                <div className="flex items-center space-x-4">
+                <SignedOut>
+                    <SignInButton mode="modal">
+                    <Button variant="outline" className="border-blue-600 text-blue-600 hover:bg-blue-50">
+                        Sign In
+                    </Button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton afterSignOutUrl="/" />
+                </SignedIn>
+                </div>
+            </nav>
         </div>
     )
 }
